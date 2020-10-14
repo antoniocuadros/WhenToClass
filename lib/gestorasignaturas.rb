3e497264c1cb6485713e9f84bcea25ef13abb893
Antonio Cuadros Lapresta <antculap@gmail.com>
2020-10-14 22:10:48 +0200

    Avanza #26 ya que definición clase GestorAsignaturas

---
+++ lib/gestorasignaturas.rb
@@ -0,0 +1,15 @@
+require_relative "horarioasignatura.rb"
+require_relative "asignatura.rb"
+
+
+##############################################################################################
+#												#
+#					Clase GestorAsignaturas				#
+#												#
+##############################################################################################
+
+class GestorAsignaturas
+
+
+
+end
