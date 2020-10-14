ee5baf0d59b22725d5bf681a1e36295aee6ca23e
Antonio Cuadros Lapresta <antculap@gmail.com>
2020-10-14 10:44:51 +0200

    Avanza #14 ya que sujetos primer test añadidos

--- t/TestAsignaturas.rb
+++ t/TestAsignaturas.rb
@@ -17,6 +17,21 @@ class TestAsignaturas < Minitest::Test
 					[["28sep - 2oct", "12oct - 16oct", "26oct - 30oct", "9nov - 13nov", "23nov - 27nov", "7dec - 11dec", "21dec - 22dec"],
 					["5oct - 9oct", "19oct - 23oct", "2nov - 6nov","16nov - 20nov", "30nov - 4dec", "14dec - 18dec", "8jan y 11jan - 14jan"]],
 					)
+	@asignaturaSinHorarioTeoria = Asignatura.new("Infraestructura Virtual", 
+					nil, 
+					[HorarioAsignatura.new("2-Martes", "9:30", "11:30", "P1"), 
+					HorarioAsignatura.new("5-Viernes", "9:30", "11:30", "P2")],
+					"A",
+					[["28sep - 2oct", "12oct - 16oct", "26oct - 30oct", "9nov - 13nov", "23nov - 27nov", "7dec - 11dec", "21dec - 22dec"],
+					["5oct - 9oct", "19oct - 23oct", "2nov - 6nov","16nov - 20nov", "30nov - 4dec", "14dec - 18dec", "8jan y 11jan - 14jan"]],
+					)
+	@asignaturaSinHorarioPracticas = Asignatura.new("Infraestructura Virtual", 
+					HorarioAsignatura.new("2-Martes", "11:30", "13:30", "T"), 
+					nil,
+					"A",
+					[["28sep - 2oct", "12oct - 16oct", "26oct - 30oct", "9nov - 13nov", "23nov - 27nov", "7dec - 11dec", "21dec - 22dec"],
+					["5oct - 9oct", "19oct - 23oct", "2nov - 6nov","16nov - 20nov", "30nov - 4dec", "14dec - 18dec", "8jan y 11jan - 14jan"]],
+					)
 	end
 	
 	#####################################################################################
