140273bfff87e7bd438beda4ebff1d933a5a8e1a
Antonio Cuadros Lapresta <antculap@gmail.com>
2020-10-14 13:21:08 +0200

    closes #24 ya que añadido último comentario

--- t/TestAsignaturas.rb
+++ t/TestAsignaturas.rb
@@ -146,6 +146,7 @@ class TestAsignaturas < Minitest::Test
 	#TEST 2
 	#Se comprueba que el metodo devuelve true o false en función de si hay que ir esta semana
 	def test_that_tengoQueIrEstaSemana_method_return_valores_correctos
+		#necesario ya que dependía del día actual puede cambiar el resultado
 		DateTime.stub(:now, Date.new(2020,10,13)) do
 			assert_equal true, @asignatura.tengoQueIrEstaSemana("1")
 		end
