9e23afec933239e942997b98623326ede9218466
Antonio Cuadros Lapresta <antculap@gmail.com>
2020-11-09 11:39:11 +0100

    Avanza #68 ya que clase gestorGrados añadida

---
+++ lib/gestorgrados.rb
@@ -0,0 +1,15 @@
+#Antonio Cuadros Lapresta
+require_relative "horarioasignatura.rb"
+require_relative "asignaturaerror.rb"
+##############################################################################################
+#												                                             #
+#					Clase GestorGrados					                                     #
+#												                                             #
+##############################################################################################
+class GestorGrados
+
+    def initialize()
+        @grados = Array.new
+    end
+
+end
