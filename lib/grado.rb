d8175ac0cd4426ca13583c4dfdb63a4dfa3cce35
Antonio Cuadros Lapresta <antculap@gmail.com>
2020-11-09 11:00:28 +0100

    Avanza #68 ya que clase Grado añadida

---
+++ lib/grado.rb
@@ -0,0 +1,16 @@
+require_relative "asignaturaerror.rb"
+require_relative "horarioasignatura.rb"
+require_relative "asignatura.rb"
+require_relative "gestorasignatura.rb"
+
+##############################################################################################
+#												                                             #
+#					Clase grado				                                                 #
+#												                                             #
+##############################################################################################
+
+class Grado
+
+
+
+end
