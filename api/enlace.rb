54fd9cc9ceba441c8b36861bc39186d86ea3439a
Antonio Cuadros Lapresta <antculap@gmail.com>
2020-11-20 19:39:33 +0100

    Avanza HU[6] #47 y #77 ya que añadida lectura de datos

--- api/enlace.rb
+++ api/enlace.rb
@@ -1,4 +1,28 @@
+require 'json'
+
 Handler = Proc.new do |req, res|
+  #obtenemos los parámetros
+  asignatura = req.query['asignatura']
+  modalidad = req.query['m']
+  dia = req.query['d']
+
+  #################
+  #Leemos el fichero de datos
+  if datos != nil
+    ubicacion = File.join(File.dirname(__FILE__),'data.json')
+    fichero = File.read(ubicacion)
+    datos = JSON.parse(fichero)
+  end
+
+  
+
+
+
+
+
+
+
+
   res.status = 200
 
   res['Content-Type'] = 'text/text; charset=utf-8'
