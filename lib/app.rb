b754a5dc7e7634c817ae7a8a06123507afb2d30d
Antonio Cuadros Lapresta <antculap@gmail.com>
2020-12-05 12:39:57 +0100

    Avanza [HU4] #28 ya que modificadas rutas para gestionar  asignaturas [skip ci]

--- lib/app.rb
+++ lib/app.rb
@@ -25,6 +25,38 @@ class App < Roda
         r.on "grado" do
             # /grado/$ID
             r.on /[a-f0-9]{20}/ do |id|
+                
+                r.on "asignatura" do
+                    r.on /[a-f0-9]{20}/ do |id|
+                        # /asignatura/$ID/horario
+                        r.get "horario" do
+                            "obtener el horario de la asignatura"
+                        end
+
+                        # /asignatura/$ID/enlaces
+                        r.get "enlaces" do
+                            "obtener los enlaces de la asignatura"
+                        end
+
+                        # /asignatura/$ID/turnos
+                        r.get "turnos" do
+                            "obtener los turnos de la asignatura"
+                        end
+                            
+                        # /asignatura/$ID
+                        r.get do
+                            "obtiene toda la información de una asignatura"    
+                        end
+
+                        r.delete do
+                            "elimina la asignatura"
+                        end
+                    end
+                    r.post do
+                        "añadir una asignatura"
+                    end
+                end
+                
                 r.on "asignaturas" do
                     r.get do
                         "obtener las asignaturas del grado"
@@ -41,38 +73,6 @@ class App < Roda
                 r.post do
                     "añadir grado"
                 end
-            end
-        end
-
-        # /asignatura
-        r.on "asignatura" do
-            r.on /[a-f0-9]{20}/ do |id|
-                # /asignatura/$ID/horario
-                r.get "horario" do
-                    "obtener el horario de la asignatura"
-                end
-
-                # /asignatura/$ID/enlaces
-                r.get "enlaces" do
-                    "obtener los enlaces de la asignatura"
-                end
-
-                # /asignatura/$ID/turnos
-                r.get "turnos" do
-                    "obtener los turnos de la asignatura"
-                end
-                    
-                # /asignatura/$ID
-                r.get do
-                    "obtiene toda la información de una asignatura"    
-                end
-
-                r.delete do
-                    "elimina la asignatura"
-                end
-            end
-            r.post do
-                "añadir una asignatura"
             end
         end
     end
