0f6748fbe163defa1923c073827d31394319ba49
Antonio Cuadros Lapresta <antculap@gmail.com>
2020-12-06 16:02:31 +0100

    Avanza [HU4] #28 ya que se añade método en el microservicio para añadir una asignatura [skip ci]

--- lib/app.rb
+++ lib/app.rb
@@ -75,13 +75,23 @@ class App < Roda
                         # /asignatura/$ID
                         # curl --request GET http://localhost:9292/grado/0e78a27a1e605334c0ba/asignatura/50bbd29ff87ba567f7bd
                         r.get do
-                            asignatura = @gestor.obtenerAsignatura(id, id2)
-                            asigjson = @parse.asignaturaToJSON(asignatura)
+                            begin
+                                asignatura = @gestor.obtenerAsignatura(id, id2)
+                                asigjson = @parse.asignaturaToJSON(asignatura)
 
-                            #Preparamos la respuesta
-                            response.status = 200
-                            response['Content-Type'] = 'application/json'
-                            response.write(asigjson.to_json)
+                                #Preparamos la respuesta
+                                response.status = 200
+                                response['Content-Type'] = 'application/json'
+                                response.write(asigjson.to_json)
+                            rescue => exception
+                                response.status = 404
+                                response['Content-Type'] = 'application/json'
+                                res = {
+                                    "error"=>"No existe el grado o la asignatura"
+                                }
+                                response.write(res.to_json)    
+                            end
+                            
                         end
 
                         r.delete do
@@ -103,8 +113,32 @@ class App < Roda
                             end
                         end
                     end
+
+                    # post /grado/$ID/asignatura
+                    # curl --header "Content-Type:application/json" --request POST --data '{"id":"50bbd29ff87ba567f7bd","siglas":"IV","nombre":"Infraestructura Virtual","horario_teoria":{"dia":"2-Martes","hora_inicio":"11:30","hora_fin":"13:30","grupo":"T"},"horario_practicas":[{"dia":"2-Martes","hora_inicio":"9:30","hora_fin":"11:30","grupo":"P1"},{"dia":"5-Viernes","hora_inicio":"9:30","hora_fin":"11:30","grupo":"P2"}],"turno_presencialidad":[["28sep - 2oct","12oct - 16oct","26oct - 30oct","9nov - 13nov","23nov - 27nov","7dec - 11dec","21dec - 22dec"],["5oct - 9oct","19oct - 23oct","2nov - 6nov","16nov - 20nov","30nov - 4dec","14dec - 18dec","8jan y 11jan - 14jan"]],"grupo":"A","enlaces_clase_online":["https://meet.jit.si/IV-ETSIIT-UGR-2020","https://meet.jit.si/IV-ETSIIT-UGR-2020","https://meet.jit.si/IV-ETSIIT-UGR-2020"],"curso":"4"}' http://localhost:9292/grado/0e78a27a1e605334c0ba/asignatura
                     r.post do
-                        "añadir una asignatura"
+                        begin
+                            asignatura = JSON.parse(r.body.read)
+            
+                            parsed = @parse.jsonToAsignatura(asignatura)
+    
+                            @gestor.añadeAsignatura(id ,parsed)
+    
+                            #Preparamos la respuesta
+                            response.status = 200
+                            response['Content-Type'] = 'application/json'
+                            res = {
+                                "añadido"=>parsed.id
+                            }
+                            response.write(res.to_json) 
+                        rescue => exception
+                            response.status = 404
+                            response['Content-Type'] = 'application/json'
+                            res = {
+                                "error"=>"No existe el grado o la asignatura"
+                            }
+                            response.write(res.to_json)
+                        end
                     end
                 end
 
