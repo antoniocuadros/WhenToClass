8f6586e424e0a04d5c5e4232707ef149d278cd69
Antonio Cuadros Lapresta <antculap@gmail.com>
2020-12-07 13:26:19 +0100

    Avanza [HU10] #71 ya que añadidos t tests metodo  microservicio eliminar grado [skip ci]

--- t/TestApi.rb
+++ t/TestApi.rb
@@ -60,4 +60,34 @@ class TestApi < Minitest::Test
         delete '/grado/0e78a27a1e605334c0ba' #borramos para no tener conflictos en sucesivos tests
     end
 
+    #####################################################################################
+	#Tests que comprueba que funciona correctamente el método para eliminar grado
+	#
+	#método: eliminar grado en el microservicio
+	#HU10
+	#####################################################################################
+    #Test 1: Eliminar correctamente
+    def test_eliminar_grado_ok
+        a_anadir = {"id"=>"0e78a27a1e605334c0ba","asignaturas"=>[{"id"=>"50bbd28fa87ba567f7bd","siglas"=>"IV","nombre"=>"Infraestructura Virtual","horario_teoria"=>{"dia"=>"2-Martes","hora_inicio"=>"11:30","hora_fin"=>"13:30","grupo"=>"T"},"horario_practicas"=>[{"dia"=>"2-Martes","hora_inicio"=>"9:30","hora_fin"=>"11:30","grupo"=>"P1"},{"dia"=>"5-Viernes","hora_inicio"=>"9:30","hora_fin"=>"11:30","grupo"=>"P2"}],"turno_presencialidad"=>[["28sep - 2oct","12oct - 16oct","26oct - 30oct","9nov - 13nov","23nov - 27nov","7dec - 11dec","21dec - 22dec"],["5oct - 9oct","19oct - 23oct","2nov - 6nov","16nov - 20nov","30nov - 4dec","14dec - 18dec","8jan y 11jan - 14jan"]],"grupo"=>"A","enlaces_clase_online"=>["https://meet.jit.si/IV-ETSIIT-UGR-2020","https://meet.jit.si/IV-ETSIIT-UGR-2020","https://meet.jit.si/IV-ETSIIT-UGR-2020"],"curso"=>"4"}],"nombre_grado"=>"Ingeniería Informática","enlace_grado"=>"https://grados.ugr.es/informatica/"}
+        #Añadimos para poder borrar
+        post '/grado', a_anadir.to_json    
+
+        #Probamos a borrar
+        delete '/grado/0e78a27a1e605334c0ba' 
+            res = {"eliminado"=>"0e78a27a1e605334c0ba"}
+            res = res.to_json
+            assert_equal last_response.status, 200
+            assert_equal(last_response.content_type, 'application/json')
+            assert_equal(last_response.body, res)
+    end
+
+    #Test 2: Eliminar falla si no existe lo que se quiere borrar
+    def test_eliminar_grado_falla
+        delete '/grado/0e78a27a1e605334c0ba' 
+            res = {"error"=>"No existe el grado"}
+            res = res.to_json
+            assert_equal last_response.status, 404
+            assert_equal(last_response.content_type, 'application/json')
+            assert_equal(last_response.body, res)
+    end
 end
