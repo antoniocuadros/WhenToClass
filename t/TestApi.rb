ea15efc846b55a455a9c5db9aefb571d8bdde8fa
Antonio Cuadros Lapresta <antculap@gmail.com>
2020-12-07 14:49:01 +0100

    Avanza [HU4] #28 ya que mejorados tests microservicio

--- t/TestApi.rb
+++ t/TestApi.rb
@@ -413,7 +413,7 @@ class TestApi < Minitest::Test
     #####################################################################################
     #Test 1: Se obtienen asignaturas correctamente
     def test_obtiene_todas_asignaturas_ok
-        a_anadir = {"id"=>"0e78a27a1e605334c0ba","asignaturas"=>[{"id"=>"50bbd28fa87ba567f7bd","siglas"=>"IV","nombre"=>"Infraestructura Virtual","horario_teoria"=>{"dia"=>"2-Martes","hora_inicio"=>"11:30","hora_fin"=>"13:30","grupo"=>"T"},"horario_practicas"=>[{"dia"=>"2-Martes","hora_inicio"=>"9:30","hora_fin"=>"11:30","grupo"=>"P1"},{"dia"=>"5-Viernes","hora_inicio"=>"9:30","hora_fin"=>"11:30","grupo"=>"P2"}],"turno_presencialidad"=>[["28sep - 2oct","12oct - 16oct","26oct - 30oct","9nov - 13nov","23nov - 27nov","7dec - 11dec","21dec - 22dec"],["5oct - 9oct","19oct - 23oct","2nov - 6nov","16nov - 20nov","30nov - 4dec","14dec - 18dec","8jan y 11jan - 14jan"]],"grupo"=>"A","enlaces_clase_online"=>["https://meet.jit.si/IV-ETSIIT-UGR-2020","https://meet.jit.si/IV-ETSIIT-UGR-2020","https://meet.jit.si/IV-ETSIIT-UGR-2020"],"curso"=>"4"}],"nombre_grado"=>"Ingeniería Informática","enlace_grado"=>"https://grados.ugr.es/informatica/"}
+        a_anadir = @grado
         #Añadimos para poder consultar asignaturas
         post '/grado', a_anadir.to_json    
 
