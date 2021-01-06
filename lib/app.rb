4734f2275d3f82fe76a2a0cf7c47ade9b8eee430
Antonio Cuadros Lapresta <antculap@gmail.com>
2021-01-06 17:07:47 +0100

    Avanza [HU4] #28 ya que actualizado método api añadirasignatura con PUT ya que conocemos URI

--- lib/app.rb
+++ lib/app.rb
@@ -198,32 +198,32 @@ class App < Roda
                                 response.write(res.to_json)
                             end
                         end
-                    end
 
-                    # post /grado/$ID/asignatura
-                    # curl --header "Content-Type:application/json" --request POST --data '{"id":"50bbd29ff87ba567f7bd","siglas":"IV","nombre":"Infraestructura Virtual","horario_teoria":{"dia":"2-Martes","hora_inicio":"11:30","hora_fin":"13:30","grupo":"T"},"horario_practicas":[{"dia":"2-Martes","hora_inicio":"9:30","hora_fin":"11:30","grupo":"P1"},{"dia":"5-Viernes","hora_inicio":"9:30","hora_fin":"11:30","grupo":"P2"}],"turno_presencialidad":[["28sep - 2oct","12oct - 16oct","26oct - 30oct","9nov - 13nov","23nov - 27nov","7dec - 11dec","21dec - 22dec"],["5oct - 9oct","19oct - 23oct","2nov - 6nov","16nov - 20nov","30nov - 4dec","14dec - 18dec","8jan y 11jan - 14jan"]],"grupo":"A","enlaces_clase_online":["https://meet.jit.si/IV-ETSIIT-UGR-2020","https://meet.jit.si/IV-ETSIIT-UGR-2020","https://meet.jit.si/IV-ETSIIT-UGR-2020"],"curso":"4"}' http://localhost:9292/grado/0e78a27a1e605334c0ba/asignatura
-                    r.post do
-                        begin
-                            asignatura = JSON.parse(r.body.read)
-            
-                            parsed = @parse.jsonToAsignatura(asignatura)
-    
-                            @gestor.añadeAsignatura(id ,parsed)
-    
-                            #Preparamos la respuesta
-                            response.status = 200
-                            response['Content-Type'] = 'application/json'
-                            res = {
-                                "añadido"=>parsed.id
-                            }
-                            response.write(res.to_json) 
-                        rescue
-                            response.status = 404
-                            response['Content-Type'] = 'application/json'
-                            res = {
-                                "error"=>"No existe el grado o la asignatura"
-                            }
-                            response.write(res.to_json)
+                        # put /grado/$ID/asignatura/$ID2
+                        # curl --header "Content-Type:application/json" --request PUT --data '{"id":"50bbd29ff87ba567f7bd","siglas":"IV","nombre":"Infraestructura Virtual","horario_teoria":{"dia":"2-Martes","hora_inicio":"11:30","hora_fin":"13:30","grupo":"T"},"horario_practicas":[{"dia":"2-Martes","hora_inicio":"9:30","hora_fin":"11:30","grupo":"P1"},{"dia":"5-Viernes","hora_inicio":"9:30","hora_fin":"11:30","grupo":"P2"}],"turno_presencialidad":[["28sep - 2oct","12oct - 16oct","26oct - 30oct","9nov - 13nov","23nov - 27nov","7dec - 11dec","21dec - 22dec"],["5oct - 9oct","19oct - 23oct","2nov - 6nov","16nov - 20nov","30nov - 4dec","14dec - 18dec","8jan y 11jan - 14jan"]],"grupo":"A","enlaces_clase_online":["https://meet.jit.si/IV-ETSIIT-UGR-2020","https://meet.jit.si/IV-ETSIIT-UGR-2020","https://meet.jit.si/IV-ETSIIT-UGR-2020"],"curso":"4"}' http://localhost:9292/grado/0e78a27a1e605334c0ba/asignatura/50bbd29ff87ba567f7bd
+                        r.put do
+                            begin
+                                asignatura = JSON.parse(r.body.read)
+                
+                                parsed = @parse.jsonToAsignatura(asignatura)
+        
+                                @gestor.añadeAsignatura(id ,parsed)
+        
+                                #Preparamos la respuesta
+                                response.status = 200
+                                response['Content-Type'] = 'application/json'
+                                res = {
+                                    "añadido"=>parsed.id
+                                }
+                                response.write(res.to_json) 
+                            rescue
+                                response.status = 404
+                                response['Content-Type'] = 'application/json'
+                                res = {
+                                    "error"=>"No existe el grado o la asignatura"
+                                }
+                                response.write(res.to_json)
+                            end
                         end
                     end
                 end
--- t/TestApi.rb
+++ t/TestApi.rb
@@ -191,7 +191,7 @@ class TestApi < Minitest::Test
 
         #Probamos a añadir
         a_anadir = {"id"=>"50bbd28fa87ba567f7bd","siglas"=>"IV","nombre"=>"Infraestructura Virtual","horario_teoria"=>{"dia"=>"2-Martes","hora_inicio"=>"11:30","hora_fin"=>"13:30","grupo"=>"T"},"horario_practicas"=>[{"dia"=>"2-Martes","hora_inicio"=>"9:30","hora_fin"=>"11:30","grupo"=>"P1"},{"dia"=>"5-Viernes","hora_inicio"=>"9:30","hora_fin"=>"11:30","grupo"=>"P2"}],"turno_presencialidad"=>[["28sep - 2oct","12oct - 16oct","26oct - 30oct","9nov - 13nov","23nov - 27nov","7dec - 11dec","21dec - 22dec"],["5oct - 9oct","19oct - 23oct","2nov - 6nov","16nov - 20nov","30nov - 4dec","14dec - 18dec","8jan y 11jan - 14jan"]],"grupo"=>"A","enlaces_clase_online"=>["https://meet.jit.si/IV-ETSIIT-UGR-2020","https://meet.jit.si/IV-ETSIIT-UGR-2020","https://meet.jit.si/IV-ETSIIT-UGR-2020"],"curso"=>"4"}
-        post '/grado/0e78a27a1e605334c0ba/asignatura/', a_anadir.to_json 
+        put '/grado/0e78a27a1e605334c0ba/asignatura/50bbd28fa87ba567f7bd', a_anadir.to_json 
             res = {"añadido"=>"50bbd28fa87ba567f7bd"}
             res = res.to_json
             assert_equal last_response.status, 200
@@ -204,7 +204,7 @@ class TestApi < Minitest::Test
     def test_añade_asignatura_falla
         #Probamos a añadir
         a_anadir = @asignatura
-        post '/grado/0e78a27a1e605334c0ba/asignatura/', a_anadir.to_json 
+        put '/grado/0e78a27a1e605334c0ba/asignatura/50bbd28fa87ba567f7bd', a_anadir.to_json 
             res = {"error"=>"No existe el grado o la asignatura"}
             res = res.to_json
             assert_equal last_response.status, 404
