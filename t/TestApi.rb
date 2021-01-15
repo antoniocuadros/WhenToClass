24e7992207e4af2df4498253188ed47489f8e09e
Antonio Cuadros Lapresta <antculap@gmail.com>
2021-01-15 10:52:59 +0100

    Avanza #103 y HU[10] #71 ya que adaptados tests a cambio de nombre de rutas

--- t/TestApi.rb
+++ t/TestApi.rb
@@ -50,13 +50,13 @@ class TestApi < Minitest::Test
     def test_anadir_grado_ok
     MONGODator.stub(:new, FSDator.new("../data")) do
         a_anadir = @grado
-        put '/grado/0e78a27a1e605334c0ba', a_anadir.to_json    
+        put '/grados/0e78a27a1e605334c0ba', a_anadir.to_json    
             res = {"añadido"=>"0e78a27a1e605334c0ba"}
             res = res.to_json
             assert_equal(last_response.content_type, 'application/json')
-            assert_equal(last_response.location, '/grado/0e78a27a1e605334c0ba')
+            assert_equal(last_response.location, '/grados/0e78a27a1e605334c0ba')
             assert_equal(last_response.body, res)
-        delete '/grado/0e78a27a1e605334c0ba' 
+        delete '/grados/0e78a27a1e605334c0ba' 
     end
     end
 
@@ -73,10 +73,10 @@ class TestApi < Minitest::Test
     MONGODator.stub(:new, FSDator.new("../data")) do
         a_anadir = @grado
         #Añadimos para poder borrar
-        put '/grado/0e78a27a1e605334c0ba', a_anadir.to_json    
+        put '/grados/0e78a27a1e605334c0ba', a_anadir.to_json    
 
         #Probamos a borrar
-        delete '/grado/0e78a27a1e605334c0ba' 
+        delete '/grados/0e78a27a1e605334c0ba' 
             res = {"eliminado"=>"0e78a27a1e605334c0ba"}
             res = res.to_json
             assert_equal last_response.status, 200
@@ -88,7 +88,7 @@ class TestApi < Minitest::Test
     #Test 2: Eliminar falla si no existe lo que se quiere borrar
     def test_eliminar_grado_falla
     MONGODator.stub(:new, FSDator.new("../data")) do
-        delete '/grado/123' 
+        delete '/grados/123' 
             res = {"error"=>"No existe el grado"}
             res = res.to_json
             assert_equal last_response.status, 404
@@ -108,16 +108,16 @@ class TestApi < Minitest::Test
     MONGODator.stub(:new, FSDator.new("../data")) do
         a_anadir = @grado
         #Añadimos para poder consultar
-        put '/grado/0e78a27a1e605334c0ba', a_anadir.to_json    
+        put '/grados/0e78a27a1e605334c0ba', a_anadir.to_json    
 
         #Probamos a consultar
-        get '/grado/0e78a27a1e605334c0ba' 
+        get '/grados/0e78a27a1e605334c0ba' 
             res = a_anadir
             res = res.to_json
             assert_equal last_response.status, 200
             assert_equal(last_response.content_type, 'application/json')
             assert_equal(last_response.body, res)
-        delete '/grado/0e78a27a1e605334c0ba' 
+        delete '/grados/0e78a27a1e605334c0ba' 
     end
     end
 
@@ -125,7 +125,7 @@ class TestApi < Minitest::Test
     def test_consultar_grado_falla
     MONGODator.stub(:new, FSDator.new("../data")) do
         #Probamos a consultar
-        get '/grado/0e78a27a1e605334c0ba' 
+        get '/grados/0e78a27a1e605334c0ba' 
             res = {"error"=>"No existe el grado"}
             res = res.to_json
             assert_equal last_response.status, 404
@@ -145,16 +145,16 @@ class TestApi < Minitest::Test
     MONGODator.stub(:new, FSDator.new("../data")) do
         a_anadir = @grado
         #Añadimos para poder consultar
-        put '/grado/0e78a27a1e605334c0ba', a_anadir.to_json    
+        put '/grados/0e78a27a1e605334c0ba', a_anadir.to_json    
 
         #Probamos a consultar
-        get '/grado/0e78a27a1e605334c0ba/asignatura/50bbd28fa87ba567f7bd' 
+        get '/grados/0e78a27a1e605334c0ba/asignatura/50bbd28fa87ba567f7bd' 
             res = @asignatura
             res = res.to_json
             assert_equal last_response.status, 200
             assert_equal(last_response.content_type, 'application/json')
             assert_equal(last_response.body, res)
-        delete '/grado/0e78a27a1e605334c0ba' 
+        delete '/grados/0e78a27a1e605334c0ba' 
     end
     end
 
@@ -162,7 +162,7 @@ class TestApi < Minitest::Test
     def test_consultar_asignatura_falla
     MONGODator.stub(:new, FSDator.new("../data")) do
         #Probamos a consultar
-        get '/grado/0e78a27a1e605334c0ba/asignatura/123' 
+        get '/grados/0e78a27a1e605334c0ba/asignatura/123' 
             res = {"error"=>"No existe el grado o la asignatura"}
             res = res.to_json
             assert_equal last_response.status, 404
@@ -182,16 +182,16 @@ class TestApi < Minitest::Test
     MONGODator.stub(:new, FSDator.new("../data")) do
         a_anadir = @grado
         #Añadimos para poder eliminar
-        put '/grado/0e78a27a1e605334c0ba', a_anadir.to_json    
+        put '/grados/0e78a27a1e605334c0ba', a_anadir.to_json    
 
         #Probamos a eliminar
-        delete '/grado/0e78a27a1e605334c0ba/asignatura/50bbd28fa87ba567f7bd' 
+        delete '/grados/0e78a27a1e605334c0ba/asignatura/50bbd28fa87ba567f7bd' 
             res = {"eliminado"=>"50bbd28fa87ba567f7bd"}
             res = res.to_json
             assert_equal last_response.status, 200
             assert_equal(last_response.content_type, 'application/json')
             assert_equal(last_response.body, res)
-        delete '/grado/0e78a27a1e605334c0ba' 
+        delete '/grados/0e78a27a1e605334c0ba' 
     end   
     end
 
@@ -199,7 +199,7 @@ class TestApi < Minitest::Test
     def test_elimina_asignatura_falla
     MONGODator.stub(:new, FSDator.new("../data")) do    
         #Probamos a eliminar
-        delete '/grado/0e78a27a1e605334c0ba/asignatura/50bbd28fa87ba567f7bd' 
+        delete '/grados/0e78a27a1e605334c0ba/asignatura/50bbd28fa87ba567f7bd' 
             res = {"error"=>"No existe el grado o la asignatura"}
             res = res.to_json
             assert_equal last_response.status, 404 
@@ -218,18 +218,18 @@ class TestApi < Minitest::Test
     MONGODator.stub(:new, FSDator.new("../data")) do
         a_anadir = {"id"=>"0e78a27a1e605334c0ba","asignaturas"=>[],"nombre_grado"=>"Ingeniería Informática","enlace_grado"=>"https://grados.ugr.es/informatica/"}
         #Añadimos grado para poder añadir asignaturas
-        put '/grado/0e78a27a1e605334c0ba', a_anadir.to_json    
+        put '/grados/0e78a27a1e605334c0ba', a_anadir.to_json    
 
         #Probamos a añadir
         a_anadir = {"id"=>"50bbd28fa87ba567f7bd","siglas"=>"IV","nombre"=>"Infraestructura Virtual","horario_teoria"=>{"dia"=>"2-Martes","hora_inicio"=>"11:30","hora_fin"=>"13:30","grupo"=>"T"},"horario_practicas"=>[{"dia"=>"2-Martes","hora_inicio"=>"9:30","hora_fin"=>"11:30","grupo"=>"P1"},{"dia"=>"5-Viernes","hora_inicio"=>"9:30","hora_fin"=>"11:30","grupo"=>"P2"}],"turno_presencialidad"=>[["28sep - 2oct","12oct - 16oct","26oct - 30oct","9nov - 13nov","23nov - 27nov","7dec - 11dec","21dec - 22dec"],["5oct - 9oct","19oct - 23oct","2nov - 6nov","16nov - 20nov","30nov - 4dec","14dec - 18dec","8jan y 11jan - 14jan"]],"grupo"=>"A","enlaces_clase_online"=>["https://meet.jit.si/IV-ETSIIT-UGR-2020","https://meet.jit.si/IV-ETSIIT-UGR-2020","https://meet.jit.si/IV-ETSIIT-UGR-2020"],"curso"=>"4"}
-        put '/grado/0e78a27a1e605334c0ba/asignatura/50bbd28fa87ba567f7bd', a_anadir.to_json 
+        put '/grados/0e78a27a1e605334c0ba/asignatura/50bbd28fa87ba567f7bd', a_anadir.to_json 
             res = {"añadido"=>"50bbd28fa87ba567f7bd"}
             res = res.to_json
             assert_equal last_response.status, 200
             assert_equal(last_response.content_type, 'application/json')
-            assert_equal(last_response.location, '/grado/0e78a27a1e605334c0ba/asignatura/50bbd28fa87ba567f7bd')
+            assert_equal(last_response.location, '/grados/0e78a27a1e605334c0ba/asignatura/50bbd28fa87ba567f7bd')
             assert_equal(last_response.body, res)
-        delete '/grado/0e78a27a1e605334c0ba' 
+        delete '/grados/0e78a27a1e605334c0ba' 
     end
     end
 
@@ -238,7 +238,7 @@ class TestApi < Minitest::Test
     MONGODator.stub(:new, FSDator.new("../data")) do
         #Probamos a añadir
         a_anadir = @asignatura
-        put '/grado/0e78a27a1e605334c0ba/asignatura/50bbd28fa87ba567f7bd', a_anadir.to_json 
+        put '/grados/0e78a27a1e605334c0ba/asignatura/50bbd28fa87ba567f7bd', a_anadir.to_json 
             res = {"error"=>"No existe el grado o la asignatura"}
             res = res.to_json
             assert_equal last_response.status, 404
@@ -258,16 +258,16 @@ class TestApi < Minitest::Test
     MONGODator.stub(:new, FSDator.new("../data")) do
         a_anadir = @grado
         #Añadimos para poder consultar horario
-        put '/grado/0e78a27a1e605334c0ba', a_anadir.to_json    
+        put '/grados/0e78a27a1e605334c0ba', a_anadir.to_json    
 
         #Probamos a consultar horario
-        get '/grado/0e78a27a1e605334c0ba/asignatura/50bbd28fa87ba567f7bd/horario?grupo=P1' 
+        get '/grados/0e78a27a1e605334c0ba/asignatura/50bbd28fa87ba567f7bd/horario?grupo=P1' 
             res = {"horario"=>[{"dia"=>"2-Martes","hora_inicio"=>"11:30","hora_fin"=>"13:30","grupo"=>"T"}, {"dia"=>"2-Martes","hora_inicio"=>"9:30","hora_fin"=>"11:30","grupo"=>"P1"}]}
             res = res.to_json
             assert_equal last_response.status, 200
             assert_equal(last_response.content_type, 'application/json')
             assert_equal(last_response.body, res)
-        delete '/grado/0e78a27a1e605334c0ba' 
+        delete '/grados/0e78a27a1e605334c0ba' 
     end
     end
 
@@ -276,16 +276,16 @@ class TestApi < Minitest::Test
     MONGODator.stub(:new, FSDator.new("../data")) do
         a_anadir = @grado
         #Añadimos para poder consultar horario
-        put '/grado/0e78a27a1e605334c0ba', a_anadir.to_json    
+        put '/grados/0e78a27a1e605334c0ba', a_anadir.to_json    
 
         #Probamos a consultar horario
-        get '/grado/0e78a27a1e605334c0ba/asignatura/50bbd28fa87ba567f7bd/horario' 
+        get '/grados/0e78a27a1e605334c0ba/asignatura/50bbd28fa87ba567f7bd/horario' 
             res = {"error"=>"es necesario pasar la variable grupo"}
             res = res.to_json
             assert_equal last_response.status, 404
             assert_equal(last_response.content_type, 'application/json')
             assert_equal(last_response.body, res)
-        delete '/grado/0e78a27a1e605334c0ba' 
+        delete '/grados/0e78a27a1e605334c0ba' 
     end
     end
 
@@ -293,7 +293,7 @@ class TestApi < Minitest::Test
     def test_obtiene_horario_asignatura_falla
     MONGODator.stub(:new, FSDator.new("../data")) do    
         #Probamos a consultar horario
-        get '/grado/0e78a27a1e605334c0ba/asignatura/50bbd28fa87ba567f7bd/horario?grupo=P1'
+        get '/grados/0e78a27a1e605334c0ba/asignatura/50bbd28fa87ba567f7bd/horario?grupo=P1'
             res = {"error"=>"No se ha encontrado el grado o asignatura"}
             res = res.to_json
             assert_equal last_response.status, 404
@@ -313,16 +313,16 @@ class TestApi < Minitest::Test
     MONGODator.stub(:new, FSDator.new("../data")) do
         a_anadir = @grado
         #Añadimos para poder consultar enlaces
-        put '/grado/0e78a27a1e605334c0ba', a_anadir.to_json    
+        put '/grados/0e78a27a1e605334c0ba', a_anadir.to_json    
 
         #Probamos a consultar enlaces
-        get '/grado/0e78a27a1e605334c0ba/asignatura/50bbd28fa87ba567f7bd/enlace?grupo=P1' 
+        get '/grados/0e78a27a1e605334c0ba/asignatura/50bbd28fa87ba567f7bd/enlace?grupo=P1' 
             res = {"enlace"=>"https://meet.jit.si/IV-ETSIIT-UGR-2020"}
             res = res.to_json
             assert_equal last_response.status, 200
             assert_equal(last_response.content_type, 'application/json')
             assert_equal(last_response.body, res)
-        delete '/grado/0e78a27a1e605334c0ba' 
+        delete '/grados/0e78a27a1e605334c0ba' 
     end
     end
 
@@ -331,16 +331,16 @@ class TestApi < Minitest::Test
     MONGODator.stub(:new, FSDator.new("../data")) do
         a_anadir = @grado
         #Añadimos para poder consultar enlaces
-        put '/grado/0e78a27a1e605334c0ba', a_anadir.to_json    
+        put '/grados/0e78a27a1e605334c0ba', a_anadir.to_json    
 
         #Probamos a consultar enlaces
-        get '/grado/0e78a27a1e605334c0ba/asignatura/50bbd28fa87ba567f7bd/enlace' 
+        get '/grados/0e78a27a1e605334c0ba/asignatura/50bbd28fa87ba567f7bd/enlace' 
             res = {"error"=>"es necesario pasar la variable grupo"}
             res = res.to_json
             assert_equal last_response.status, 404
             assert_equal(last_response.content_type, 'application/json')
             assert_equal(last_response.body, res)
-        delete '/grado/0e78a27a1e605334c0ba'  
+        delete '/grados/0e78a27a1e605334c0ba'  
     end  
     end
 
@@ -348,7 +348,7 @@ class TestApi < Minitest::Test
     def test_obtiene_enlaces_asignatura_falla
     MONGODator.stub(:new, FSDator.new("../data")) do
         #Probamos a consultar enlaces
-        get '/grado/0e78a27a1e605334c0ba/asignatura/50bbd28fa87ba567f7bd/enlace?grupo=P1'
+        get '/grados/0e78a27a1e605334c0ba/asignatura/50bbd28fa87ba567f7bd/enlace?grupo=P1'
             res = {"error"=>"No se ha encontrado el grado o asignatura"}
             res = res.to_json
             assert_equal last_response.status, 404
@@ -368,16 +368,16 @@ class TestApi < Minitest::Test
     MONGODator.stub(:new, FSDator.new("../data")) do
         a_anadir = @grado
         #Añadimos para poder consultar turnos
-        put '/grado/0e78a27a1e605334c0ba', a_anadir.to_json    
+        put '/grados/0e78a27a1e605334c0ba', a_anadir.to_json    
 
         #Probamos a consultar turnos
-        get '/grado/0e78a27a1e605334c0ba/asignatura/50bbd28fa87ba567f7bd/turno?turno=1&mes=dec' 
+        get '/grados/0e78a27a1e605334c0ba/asignatura/50bbd28fa87ba567f7bd/turno?turno=1&mes=dec' 
             res = {"turnos"=>["7dec - 11dec", "21dec - 22dec"]}
             res = res.to_json
             assert_equal last_response.status, 200
             assert_equal(last_response.content_type, 'application/json')
             assert_equal(last_response.body, res)
-        delete '/grado/0e78a27a1e605334c0ba' 
+        delete '/grados/0e78a27a1e605334c0ba' 
     end
     end
 
@@ -386,16 +386,16 @@ class TestApi < Minitest::Test
     MONGODator.stub(:new, FSDator.new("../data")) do
         a_anadir = @grado
         #Añadimos para poder consultar turnos
-        put '/grado/0e78a27a1e605334c0ba', a_anadir.to_json    
+        put '/grados/0e78a27a1e605334c0ba', a_anadir.to_json    
 
         #Probamos a consultar turnos
-        get '/grado/0e78a27a1e605334c0ba/asignatura/50bbd28fa87ba567f7bd/turno' 
+        get '/grados/0e78a27a1e605334c0ba/asignatura/50bbd28fa87ba567f7bd/turno' 
             res = {"error"=>"es necesario pasar la variable turno y mes"}
             res = res.to_json
             assert_equal last_response.status, 404
             assert_equal(last_response.content_type, 'application/json')
             assert_equal(last_response.body, res)
-        delete '/grado/0e78a27a1e605334c0ba' 
+        delete '/grados/0e78a27a1e605334c0ba' 
     end
     end
 
@@ -403,7 +403,7 @@ class TestApi < Minitest::Test
     def test_obtiene_turnos_asignatura_falla
     MONGODator.stub(:new, FSDator.new("../data")) do
         #Probamos a consultar turnos
-        get '/grado/0e78a27a1e605334c0ba/asignatura/50bbd28fa87ba567f7bd/turno?turno=1&mes=dec'
+        get '/grados/0e78a27a1e605334c0ba/asignatura/50bbd28fa87ba567f7bd/turno?turno=1&mes=dec'
             res = {"error"=>"No se ha encontrado el grado o asignatura"}
             res = res.to_json
             assert_equal last_response.status, 404
@@ -423,7 +423,7 @@ class TestApi < Minitest::Test
     MONGODator.stub(:new, FSDator.new("../data")) do
         a_anadir = @grado
         #Añadimos para poder consultar grados
-        put '/grado/0e78a27a1e605334c0ba', a_anadir.to_json    
+        put '/grados/0e78a27a1e605334c0ba', a_anadir.to_json    
 
         #Probamos a consultar grados
         get '/grados' 
@@ -432,7 +432,7 @@ class TestApi < Minitest::Test
             assert_equal last_response.status, 200
             assert_equal(last_response.content_type, 'application/json')
             assert_equal(last_response.body, res)
-        delete '/grado/0e78a27a1e605334c0ba' 
+        delete '/grados/0e78a27a1e605334c0ba' 
     end
     end
 
@@ -448,16 +448,16 @@ class TestApi < Minitest::Test
     MONGODator.stub(:new, FSDator.new("../data")) do
         a_anadir = @grado
         #Añadimos para poder consultar asignaturas
-        put '/grado/0e78a27a1e605334c0ba', a_anadir.to_json    
+        put '/grados/0e78a27a1e605334c0ba', a_anadir.to_json    
 
         #Probamos a consultar asignaturas
-        get '/grado/0e78a27a1e605334c0ba/asignaturas' 
+        get '/grados/0e78a27a1e605334c0ba/asignaturas' 
             res = [{"id"=>"50bbd28fa87ba567f7bd","siglas"=>"IV","nombre"=>"Infraestructura Virtual","horario_teoria"=>{"dia"=>"2-Martes","hora_inicio"=>"11:30","hora_fin"=>"13:30","grupo"=>"T"},"horario_practicas"=>[{"dia"=>"2-Martes","hora_inicio"=>"9:30","hora_fin"=>"11:30","grupo"=>"P1"},{"dia"=>"5-Viernes","hora_inicio"=>"9:30","hora_fin"=>"11:30","grupo"=>"P2"}],"turno_presencialidad"=>[["28sep - 2oct","12oct - 16oct","26oct - 30oct","9nov - 13nov","23nov - 27nov","7dec - 11dec","21dec - 22dec"],["5oct - 9oct","19oct - 23oct","2nov - 6nov","16nov - 20nov","30nov - 4dec","14dec - 18dec","8jan y 11jan - 14jan"]],"grupo"=>"A","enlaces_clase_online"=>["https://meet.jit.si/IV-ETSIIT-UGR-2020","https://meet.jit.si/IV-ETSIIT-UGR-2020","https://meet.jit.si/IV-ETSIIT-UGR-2020"],"curso"=>"4"}]
             res = res.to_json
             assert_equal last_response.status, 200
             assert_equal(last_response.content_type, 'application/json')
             assert_equal(last_response.body, res)
-        delete '/grado/0e78a27a1e605334c0ba' 
+        delete '/grados/0e78a27a1e605334c0ba' 
     end
     end
 end
